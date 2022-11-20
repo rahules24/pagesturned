9bb0d9e2b16ec91594a51a53f0e5d3355a0d468a
Rahul <scarletteenager@outlook.com>
2022-11-21 02:41:10 +0530

    21-11

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from datetime import date
 
 
 class Poem(models.Model):
     poem_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, default="")
-    image = models.ImageField(upload_to="shop/images", default="")
+    genre = models.CharField(max_length=100, default="")
+    image = models.ImageField(upload_to="blog/images", default="")
     poem = models.TextField(max_length=10000, default="")
+    date_added = models.DateField(default=date.today())
     likes = models.IntegerField(blank=True, null=True)
     comments = models.CharField(max_length=500, default="", blank=True)
 
--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from .models import Poem
 import random
 
-
 def index(request):
-    return render(request, 'blog/index.html')
+    i = random.randrange(1, len(Poem.objects.values('poem_id')) + 1)
+    poem = Poem.objects.filter(poem_id=i)
+    snippet = Poem.objects.get(poem_id=i).poem.split('\n[2]')
+    i2 = random.randrange(1, len(Poem.objects.values('poem_id')) + 1)
+    poem2 = Poem.objects.filter(poem_id=i2)
+    snippet2 = Poem.objects.get(poem_id=i2).poem.split('\n[2]')
+    return render(request, 'blog/index.html', {'random1': poem, "snippet1": snippet, 'random2': poem2, "snippet2": snippet2})
 
 
 def blogpost(request):
