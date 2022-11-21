89bace6e257d2f9cf347cebd4111972e4ac27e49
Rahul <scarletteenager@outlook.com>
2022-11-22 01:31:00 +0530

    22-11

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Poem
 import random
 
+
 def index(request):
     i = random.randrange(1, len(Poem.objects.values('poem_id')) + 1)
     poem = Poem.objects.filter(poem_id=i)
@@ -9,10 +10,16 @@ def index(request):
     i2 = random.randrange(1, len(Poem.objects.values('poem_id')) + 1)
     poem2 = Poem.objects.filter(poem_id=i2)
     snippet2 = Poem.objects.get(poem_id=i2).poem.split('\n[2]')
-    return render(request, 'blog/index.html', {'random1': poem, "snippet1": snippet, 'random2': poem2, "snippet2": snippet2})
+    return render(request, 'blog/index.html',
+                  {'random1': poem, "snippet1": snippet, 'random2': poem2, "snippet2": snippet2})
 
 
 def blogpost(request):
-    i = random.randrange(1, len(Poem.objects.values('poem_id')) + 1)
-    poem = Poem.objects.filter(poem_id=i)
+    poem = Poem.objects.filter(poem_id=random.randrange(1, len(Poem.objects.values('poem_id')) + 1))
     return render(request, 'blog/blogpost.html', {'poems': poem})
+
+
+def poempage(request, poemid):
+    poem = Poem.objects.filter(poem_id=poemid)
+    li = Poem.objects.all()
+    return render(request, 'blog/blogpost.html', {'poems': poem, "li": li})
